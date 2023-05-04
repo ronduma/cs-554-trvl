b3393732d94de837841a2d6f87fc4e2c5ee47cfd
Ron Dumalagan <rdumalag@stevens.edu>
2023-05-04 03:32:53 -0400

    started working on register

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,10 +1,71 @@
+import React, { useState } from 'react';
+
 import '../App.css';
+import {
+  Box,
+  Button,
+  TextField,
+} from '@mui/material'
 
 function Register() {
+  const [username, setUsername] = useState(undefined);
+  const [password, setPassword] = useState(undefined);
+  const [confirmPassword, setConfirmPassword] = useState(undefined);
+
+  const handleUsername = (event) => {
+    setUsername(event.target.value);
+    console.log(username)
+  }
+
+  const handlePassword = (event) => {
+    setPassword(event.target.value);
+    console.log(password)
+  }
+
+  const handleConfirmPassword = (event) => {
+    setConfirmPassword(event.target.value);
+    console.log(confirmPassword)
+  }
+
   return (
-    <div className="Register">
-      Register
-    </div>
+    <Box
+      component="form"
+    >
+      <div>
+        Register
+      </div>
+      <div>
+        <TextField 
+          id="outlined-basic" 
+          label="Username" 
+          onChange={handleUsername}
+          variant="outlined" 
+        />
+      </div>
+      <div>
+        <TextField 
+          id="outlined-basic" 
+          label="Password" 
+          onChange={handlePassword}
+          variant="outlined" 
+        />
+      </div>
+      <div>
+        <TextField 
+          id="outlined-basic" 
+          label="Confirm Password" 
+          onChange={handleConfirmPassword}
+          variant="outlined" 
+        />
+      </div>
+      <Button
+        onClick={() => {
+          console.log(username, password, confirmPassword)
+        }}
+      >
+        Submit
+      </Button>
+    </Box>
   );
 }
 
