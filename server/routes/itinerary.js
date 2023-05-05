bff0cbc61fb3e302a6466fc67b2ce0f4f94bd533
Dylan Tran <dmtran20@gmail.com>
2023-05-04 22:50:39 -0400

    updated itineryary.js
    redis of price and location

--- server/routes/itinerary.js
+++ server/routes/itinerary.js
@@ -22,44 +22,23 @@ router.route('/').get(async (req, res) => {
 
 });
 
-// router.route('/:price').get(async (req, res) => {
-//     try {
-//         const location = req.params.location;
-//         console.log(location)
-
-//         const response = await axios.get(`https://api.yelp.com/v3/businesses/search?location=${location}`, {
-//             headers: {
-//               Authorization: `Bearer ${apiKey}`
-//             }
-//           });
-//         console.log(response)
-
-//         return res.status(200).json(response);
-
-        
-//     } catch (error) {
-        
-//         console.error(error);
-//         return res.status(500).json({ message: error});
-//     }
-
-// });
-
-router.route('/:location').get(async (req, res) => {
+router.route('/:location/:price?').get(async (req, res) => {
     console.log("getting location")
     const location = req.params.location;
+    const price = req.params.price;
     console.log(location);
+
     try {
         console.log(location)
-        const redisExist = await client.exists(`location${location}`);
+        const redisExist = await client.exists(`location${location}:price:${price || 'any'}`);
         if (redisExist){
             console.log("location id is in redis")
-            const cache = await client.get(`location${id}`);
+            const cache = await client.get(`location${location}:price:${price || 'any'}`);
             return res.status(200).json(JSON.parse(cache));
           }
         else{
-            console.log("location id does not exists in cache")
-            const response = await axios.get(`https://api.yelp.com/v3/businesses/search?location=${location}&limit=50`, {
+            console.log(`Location ${location} with price ${price} not in Redis`)
+            const response = await axios.get(`https://api.yelp.com/v3/businesses/search?location=${location}&price=${price || '1,2,3,4'}&limit=50`, {
             headers: {
               Authorization: `Bearer ${apiKey}`
             }
@@ -70,7 +49,7 @@ router.route('/:location').get(async (req, res) => {
         if (!result){
             return res.status(404).json({ message: 'No resturants in location found' });
         }
-        const cache = await client.set(`location${location}`, JSON.stringify(result));
+        const cache = await client.set(`location${location}:price:${price || 'any'}`, JSON.stringify(result));
         return res.status(200).json(result);
 
         }
