97ae5d039bd14c967aced347813b7d931f09cf49
mxfu <mphu@stevens.edu>
2023-05-06 01:10:38 -0400

    added routes and fixes to data functions + seed file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,16 @@
 const registerRoutes = require('./register');
-const itinerary = require('./itinerary')
+const itinerary = require('./itinerary');
+const postRoutes = require('./posts');
 
 const constructorMethod = (app) => {
   // app.use('/', (req, res) => {
   //   res.status(200).json("Hello World!")
   // }); 
   // app.use('/xx', xxRoutes);
-  
+
   app.use('/register', registerRoutes);
   app.use('/itinerary', itinerary);
+  app.use('/posts', postRoutes);
 
   app.use('*', (req, res) => {
     console.log('yo')
---
+++ server/routes/posts.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const users = require('../data/users');
+const helpers = require('../helpers');
+const posts = require('../data/posts');
+
+router.route('/').get(async (req, res) => {
+
+    let getAllPosts = await posts.getAllPosts();
+    return res.json(getAllPosts);
+
+})
+router.route('/:userId').post(async (req, res) => {
+    let postForm = req.body;
+    let createPost = await posts.createPost(postForm.title, req.params.userId, postForm.content);
+
+    return res.json(createPost)
+})
+router.route('/:postId').get(async (req, res) => {
+    let getPost = await posts.getPostById(req.params.postId);
+    return res.json(getPost);
+})
+router.route('/like/:postId/:userId').put(async (req, res) => {
+
+    try {
+        let likePost = await posts.likePost(req.params.postId, req.params.userId);
+        return res.json(likePost);
+    } catch (e) {
+        return res.status(400).json(e);
+    }
+})
+router.route('/reply/:postId/:userId').post(async (req, res) => {
+    let replyForm = req.body;
+    try {
+        let createComment = await posts.createReply(req.params.postId, req.params.userId, replyForm.reply);
+        return res.json(createComment);
+    } catch (e) {
+        return res.status(400).json(e);
+    }
+})
+
+//pls bring back the session.user
+router.route('/reply/:replyId/:postId/:userId').delete(async (req, res) => {
+    try {
+        let deleteReply = await posts.deleteReply(req.params.replyId, req.params.postId, req.params.userId);
+        return res.json(deleteReply);
+    } catch (e) {
+        return res.status(400).json(`${e}`);
+    }
+})
+module.exports = router;
