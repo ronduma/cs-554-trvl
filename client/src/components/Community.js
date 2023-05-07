3b9c41c82d020e4f8015656a013a04bfe3ef3ef2
Dylan Tran <dmtran20@gmail.com>
2023-05-07 12:16:32 -0400

    hotel front end
    hotel front end

--- client/src/components/Community.js
+++ client/src/components/Community.js
@@ -4,6 +4,29 @@ function Community() {
   return (
     <div className="Community">
       Community
+      
+<div class="communityContainer">
+    <div class=" cards">
+        <div class="card">
+            <h2>EXAMPLE community post title: What do I try in New York?</h2>
+            <div class="card__content">
+                <p>
+                    EXAMPLE post details: Think you're up to the challenge. Recently picked up running and trying to keep improving
+                </p>
+                <div>Posted by: user</div>
+                <div class="card_stats">
+                    <div>
+                        <p>Likes: </p>
+                    </div>
+                    <div>
+                        <p>Comments: </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    </div>
     </div>
   );
 }
