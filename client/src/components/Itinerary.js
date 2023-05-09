671bee222b737d116745dda843dbec0ced1c0632
Jae <84739325+jaejang02@users.noreply.github.com>
2023-05-09 04:10:53 -0400

    Update to main

--- client/src/components/Itinerary.js
+++ client/src/components/Itinerary.js
@@ -5,7 +5,7 @@ import '../App.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-import { Button, Card, CardActionArea, CardContent, CardMedia, Grid, Typography, 
+import { Box, Button, Card, CardActionArea, CardContent, CardMedia, Grid, Typography, 
   // makeStyles 
 } from '@mui/material';
 import actions, {handleAdd} from '../actions'
@@ -480,11 +480,34 @@ const handleOnSubmit = (collectorid, character, action) => {
      </Grid>
   )}
   
-  {/* {randomized && (
-    <Grid container spacing={5}>
-      {randomized.restaurants.map((restaurant) => buildCard(restaurant))}
-    </Grid>
-  )} */}
+  {randomized && (
+    <div>
+      <Box
+        bgcolor="lightgray"
+      >
+        <h3>Itinerary 1</h3>
+        <Grid container spacing={0}>
+          {randomized.itinerary1.map((item) => buildCard(item))}
+        </Grid>
+      </Box>
+      <Box
+        bgcolor="lightgray"
+      >
+        <h3>Itinerary 2</h3>
+        <Grid container spacing={0}>
+          {randomized.itinerary2.map((item) => buildCard(item))}
+        </Grid>
+      </Box>
+      <Box
+        bgcolor="lightgray"
+      >
+        <h3>Itinerary 3</h3>
+        <Grid container spacing={0}>
+          {randomized.itinerary3.map((item) => buildCard(item))}
+        </Grid>
+      </Box>
+    </div>   
+  )}
   </ul>
   </div>
   );
