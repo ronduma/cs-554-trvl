7e46d3525791d67ea7311d39c3cb9eb35e04c082
Jae <84739325+jaejang02@users.noreply.github.com>
2023-05-08 21:31:35 -0400

    Categories and Events done
    You can now search these itineraries

--- client/src/components/Itinerary.js
+++ client/src/components/Itinerary.js
@@ -5,7 +5,6 @@ import '../App.css';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
-
 import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography, 
   // makeStyles 
 } from '@mui/material';
@@ -47,6 +46,8 @@ function Itinerary() {
   const [YelpData, setyelpAPI] = useState([]);
   const [error, setErrorCode] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
+  const [is_free, setFree] = useState('undefined');
+  const [categories, setCategories] = useState('')
   // const classes = useStyles();
   // let card = null;
   
@@ -60,6 +61,7 @@ function Itinerary() {
   let selectedCharacters = [];
   const dispatch=useDispatch();
   // these will handle changes
+
   const handleLocationChange = (event) => {
     console.log(event.target.name)
     setLocation(event.target.value);
@@ -69,12 +71,21 @@ function Itinerary() {
     console.log(event.target.name)
     setPrice(event.target.value);
   };
+  const handleFreeChange = (event) => {
+    console.log(event.target.name);
+    setFree(event.target.value);
+  };
 
   const handleNeed = (event) => {
     console.log(event.target.name);
     setNeed(event.target.value);
   };  
-
+  const handleCategories = (event) => {
+    console.log("categories: "+ event.target.value);
+    let lowercase = (event.target.value).toLowerCase();
+    setCategories(lowercase);
+  };
+  
   if(selectedCollector !== undefined || selectedCollector !== null) {
     console.log("selectedCollector")
     console.log(selectedCollector[0].collections)
@@ -107,16 +118,32 @@ const handleOnSubmit = (collectorid, character, action) => {
     }
     else{
     try {
-      console.log("This is" + location)
+      console.log("This is " + location)
       console.log("This price" + price)
+      console.log("is Free " + is_free)
       let response;
       if (need === "hotel") {
         response = await axios.get(`http://localhost:5000/hotels/${location}/${price}`);
-      } else {
-        response = await axios.get(`http://localhost:5000/itinerary/${location}/${price}`);
+        setyelpAPI(response.data.businesses);
       }
-      console.log(response.data);
-      setyelpAPI(response.data.businesses);
+      else if(need === 'event') {
+        if(is_free === 'undefined'){
+          response = await axios.get(`http://localhost:5000/events/${location}`)
+          setyelpAPI(response.data.events);
+        }
+        else{
+          response = await axios.get(`http://localhost:5000/events/${location}/${is_free}`)
+          setyelpAPI(response.data.events); 
+        }
+      }
+      else if (need === 'category'){
+        response = await axios.get(`http://localhost:5000/categories/${location}/${categories}`); 
+        setyelpAPI(response.data.businesses);
+      } 
+      else {
+        response = await axios.get(`http://localhost:5000/itinerary/${location}/${price}`);
+        setyelpAPI(response.data.businesses);
+      }
       // const response = await axios.get(`http://localhost:5000/itinerary/${location}/${price}`);
       // console.log(response.data);
       // setyelpAPI(response.data.businesses)
@@ -236,7 +263,108 @@ const handleOnSubmit = (collectorid, character, action) => {
       </Grid>
     );
   };
+  const buildCategoriesCard = (category) => {
+    const collected = selectedCharacters.includes(category.id);
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={category.id}>
+        <Card variant='outlined'>
+        <Link to={`/itinerary/${category.id}`}>
+          <CardActionArea>
+            <CardMedia
+              component='img'
+              image={category.image_url || `No image`}
+              title={category.name}
+            />
+            
+            <CardContent>
+              <Typography gutterBottom variant='h6' component='h3'>
+                {category.name}
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                {category.location ? category.location.address1 + ', ' + category.location.city + ', ' + category.location.state : 'No location provided'}
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                {category.price ? category.price : 'No price information provided'}
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                {category.rating ? category.rating + '/5' : 'No rating information provided'}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          </Link>
+          {/* button for collecting */}
+          {collected ? (
+          <button
+            onClick={() =>
+              handleOnSubmit(selectedCollector[0], category, "giveUp")
+            }
+          >
+            Remove
+          </button>
+        ) : (
+          <button
+            onClick={() =>
+              handleOnSubmit(selectedCollector[0], category, "collect")
+            }
+          >
+            Add
+          </button>
+        )}
+        </Card>
+      </Grid>
+    );
+  };
+  const buildEventCard = (event) => {
+    const collected = selectedCharacters.includes(event.id);
+    return (
+      <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={event.id}>
+        <Card variant='outlined'>
+          <CardActionArea>
+          <Link to={`/events/${event.id}`}>
+            <CardMedia
+              component='img'
+              image={event.image_url || `No image`}
+              title={event.name}
+            />
   
+            <CardContent>
+              <Typography gutterBottom variant='h6' component='h3'>
+                {event.name}
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                {event.location ? event.location.address1 + ', ' + event.location.city + ', ' + event.location.state : 'No location provided'}
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                {event.is_free ? "Free Event" : 'Need to Pay'}
+              </Typography>
+              <Typography variant='body2' color='textSecondary' component='p'>
+                {event.rating ? event.rating + '/5' : 'No rating information provided'}
+              </Typography>
+            </CardContent>
+            </Link>
+          </CardActionArea>
+          {/* button for collecting */}
+          {collected ? (
+          <button
+            onClick={() =>
+              handleOnSubmit(selectedCollector[0], event, "giveUp")
+            }
+          >
+            Remove
+          </button>
+        ) : (
+          <button
+            onClick={() =>
+              handleOnSubmit(selectedCollector[0], event, "collect")
+            }
+          >
+            Add
+          </button>
+        )}
+        </Card>
+      </Grid>
+    );
+  };
 
   if (error) {
     return (
@@ -250,29 +378,42 @@ const handleOnSubmit = (collectorid, character, action) => {
     return (
       <div>
         <h2>Location does not exists</h2>
-        
+          
       </div>
     )
   }
   else {
-    // card =
-		// 	YelpData &&
-		// 	YelpData.map((show) => {
-		// 		return buildCard(show);
-		// 	});
   return (
     <div className='itinerary'>
       <h1>Let's Find Your Adventure Today!!!</h1>
     <div className="search-box">
       <h2>Search for a <select value={need} onChange={handleNeed}>
         <option value="restaurant">Restaurant</option>
+        <option value="category">Category</option>
         <option value="hotel">Hotel</option>
+        <option value="event">Event</option>
       </select></h2>
   <form onSubmit={handleSubmit}>
     <label className="location-label">
       Location:
       <input type="text" value={location} onChange={handleLocationChange} />
     </label>
+    {(need ==='category') ? 
+    <label className="location-label">
+      Specifcations:
+      <input type="text" value={categories} onChange={handleCategories} />
+    </label> 
+    : null}
+    {(need === 'event'  ? 
+    <label className="price-label">
+      Free:
+      <select value={is_free} onChange={handleFreeChange}>
+        <option value="undefined"> </option>
+        <option value="true">Yes</option>
+        <option value="false">No</option>
+      </select>
+    </label>
+    :
     <label className="price-label">
       Price:
       <select value={price} onChange={handlePriceChange}>
@@ -282,6 +423,7 @@ const handleOnSubmit = (collectorid, character, action) => {
         <option value="4">$$$$</option>
       </select>
     </label>
+    )}
     <button type="submit" className="search-button">Explore</button>
   </form>
   </div>
@@ -305,8 +447,18 @@ const handleOnSubmit = (collectorid, character, action) => {
          {YelpData.map((restaurant) => buildHotelCard(restaurant))}
      </Grid>
   )}
-</ul>
-    </div>
+   {YelpData && YelpData.length > 0 && need === 'event' &&(
+     <Grid container spacing={5}>
+         {YelpData.map((restaurant) => buildEventCard(restaurant))}
+     </Grid>
+  )}
+  {YelpData && YelpData.length > 0 && need === 'category' &&(
+     <Grid container spacing={5}>
+         {YelpData.map((category) => buildCategoriesCard(category))}
+     </Grid>
+  )}
+  </ul>
+  </div>
   );
 }
 
