3849ee6f6f869222a98c065e32cf03bcbd6eea30
Dylan Tran <dmtran20@gmail.com>
2023-05-09 20:37:04 -0400

    uploaded error message for imagemagick

--- server/routes/profile.js
+++ server/routes/profile.js
@@ -23,15 +23,29 @@ router.get('/', async(req,res) => {
     return res.status(200).json(userObj);
 });
 
+// router.post('/pfp', upload.single('image'), async(req,res) => {
+//     const file = req.file;
+//     if (file){
+//       console.log("saving file to /uploads")
+//       await users.saveImgToDB(req.session.user, file.path);
+//       let userObj = await users.getUserByUsername(req.session.user);
+//       await users.modifyImage(req.session.user, userObj.profilePic.buffer);
+//     }
+//     return res.redirect('/profile');
+// });
 router.post('/pfp', upload.single('image'), async(req,res) => {
-    const file = req.file;
-    if (file){
-      console.log("saving file to /uploads")
-      await users.saveImgToDB(req.session.user, file.path);
-      let userObj = await users.getUserByUsername(req.session.user);
-      await users.modifyImage(req.session.user, userObj.profilePic.buffer);
-    }
-    return res.redirect('/profile');
+  const file = req.file;
+  if (!file) {
+      return res.status(400).json({ error: "No image file uploaded" });
+  }
+  if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return res.status(400).json({ error: "Please upload a valid image file (jpg or png)" });
+  }
+  console.log("saving file to /uploads")
+  await users.saveImgToDB(req.session.user, file.path);
+  let userObj = await users.getUserByUsername(req.session.user);
+  await users.modifyImage(req.session.user, userObj.profilePic.buffer);
+  return res.redirect('/profile');
 });
 
 module.exports = router;
